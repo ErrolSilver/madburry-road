87489a933eeb90cdba94b160012fef4ee1d921f7
Errol Silver <esilver@bostoninteractive.com>
2015-06-16 22:25:18 -0400

    tpl adjustments

--- profiles/commerce_kickstart/themes/madbury_road/templates/page/page.tpl.php
+++ profiles/commerce_kickstart/themes/madbury_road/templates/page/page.tpl.php
@@ -114,11 +114,6 @@
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
@@ -129,7 +124,9 @@
       <?php if (!empty($action_links)): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
+      <?php if(drupal_is_front_page()) {unset($page['content']['system_main']['default_message']);} ?>
       <?php print render($page['content']); ?>
+
     </section>
   </div> <!-- /row -->
 </div> <!-- /container -->
